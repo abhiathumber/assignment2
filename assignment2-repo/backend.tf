98fb45a0fd9f2d1c5e9599a265df3c18665ed7fc
abhishek <99285846+abhiathumber@users.noreply.github.com>
2022-08-09 20:52:32 -0400

    Delete backend.tf

--- assignment2-repo/backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "tfstaten01512235RG"
-#     storage_account_name = "tfstaten01512235sa"
-#     container_name       = "tfstatefiles"
-#     key                  = "tfstatekey"
-#   }
-# }
